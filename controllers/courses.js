e00e6b84c3d810b563b55f43f724da0cd3c4221b
sreeni-pando <sreenivaasan@pando.in>
2020-08-23 21:06:30 +0530

    added get, create course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,9 @@
 const ErrorResponse = require('../utils/error-response');
+const Bootcamp = require('../models/Bootcamp');
 const Courses = require('../models/course');
+
 const asyncHandler = require('../middleware/async');
+
 
 exports.getCourses = asyncHandler( async (req, res, next) =>{
     let query;
@@ -22,4 +25,40 @@ exports.getCourses = asyncHandler( async (req, res, next) =>{
         data: courses,
     })
 
-})
+});
+
+exports.getCourse = asyncHandler( async (req, res, next) =>{
+    
+    const course = await Courses.findById(req.params.id).populate({
+        path:'bootcamp',
+        select: 'name description',
+    });
+
+    if(!course){
+        return next( new ErrorResponse(`Course not found with id ${req.params.id}`, 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        count: 1,
+        data: course,
+    })
+
+});
+
+exports.addCourse = asyncHandler( async (req, res, next) =>{
+    req.body.bootcamp = req.params.bootcampId;
+
+   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if(!bootcamp){
+        return next( new ErrorResponse(`bootcamp not found with id ${req.params.bootcampId}`, 404));
+    }
+    const course = await Courses.create(req.body);
+    res.status(200).json({
+        success: true,
+        count: 1,
+        data: course,
+    })
+
+});
--- routes/courses.js
+++ routes/courses.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const router = express.Router({ mergeParams: true });
 
-const { getCourses } = require('../controllers/courses');
+const { getCourses, getCourse, addCourse } = require('../controllers/courses');
 
 router.get('/', getCourses);
-
+router.get('/:id', getCourse);
+router.post('/:bootcampId/create', addCourse);
 module.exports = router;
