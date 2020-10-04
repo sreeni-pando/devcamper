351f93471b100bd9d3a7ba5cdcfb1aca91ac2cc5
sreeni-pando <sreenivaasan@pando.in>
2020-10-04 13:39:49 +0530

    added mailer

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const ErrorResponse = require('../utils/error-response');
 const User = require('../models/user');
+const crypto = require('crypto');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/send-email');
 
 exports.register = asyncHandler( async(req, res,next)=>{
     const { name, email, password, role} = req.body;
@@ -31,6 +33,56 @@ exports.login = asyncHandler( async(req, res, next)=>{
     sendTokenResponse(user, 200, res);
 })
 
+
+exports.getMe = asyncHandler(async(req, res,next)=>{
+    const user = await User.findById(req.user.id);
+    res.status(200).json({ success: true, data: user});
+});
+
+exports.forgotPassword = asyncHandler(async(req, res,next)=>{
+    const user = await User.findOne({ email: req.body.email});
+    if(!user){
+        return next( new ErrorResponse('User not exists', 404));
+    }
+    const resetToken = user.getResetPasswordToken();
+    console.log('resettoken=',resetToken);
+    await user.save({ validateBeforeSave : false})
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/resetpassword/${resetToken}`;
+    const message = `Mail to reset password ${resetUrl}`;
+    try {
+       await sendEmail({
+           email: user.email,
+           subject: 'Reset password',
+           message
+       });
+       return res.status(200).json({ success: true, data:'Email sent'})
+    } catch(err){
+        console.log(err);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+        await user.save({ validateBeforeSave: false});
+        return next(new ErrorResponse('Email not sent', 500));
+    }
+});
+exports.resetPassword = asyncHandler(async(req, res,next)=>{
+    console.log('req.params.resettoken', req.params.resettoken);
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resettoken)
+        .digest('hex');
+    console.log('resetPasswordToken', resetPasswordToken);
+    const user = await User.findOne({
+        resetPasswordToken,
+        // resetPasswordExpire :{ $gt: Date.now() }
+    });
+    if(!user){
+        return next(new ErrorResponse('Invalid Token', 400));
+    }
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+});
 const sendTokenResponse = (user, statusCode, res) =>{
     const token = user.getSignedJwtToken();
     const options = {
@@ -42,8 +94,3 @@ const sendTokenResponse = (user, statusCode, res) =>{
     }
     res.status(statusCode).cookie('token', token, options).json({ success: true, token });
 }
-
-exports.getMe = asyncHandler(async(req, res,next)=>{
-    const user = await User.findById(req.user.id);
-    res.status(200).json({ success: true, data: user});
-})
--- models/user.js
+++ models/user.js
@@ -49,6 +49,9 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function(next){
+  if(!this.isModified('password')){
+    next();
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
@@ -60,5 +63,13 @@ UserSchema.methods.getSignedJwtToken = function(){
 UserSchema.methods.matchPassword = async function(password){
   return await bcrypt.compare(password, this.password);
 }
+// generate and hash passwork token
+UserSchema.methods.getResetPasswordToken = function() {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+  this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
 
+  this.resetPasswordExpire = Date.now() + 10*60*1000;
+  console.log('resetPasswordToken', this.resetPasswordToken);
+  return this.resetPasswordToken;
+}
 module.exports = mongoose.model('User', UserSchema);
